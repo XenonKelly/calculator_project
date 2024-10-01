604ecb1efafd46cf0c174d91692cf6768b0b94cd
XenonKelly <lihocherdtova@gmail.com>
2024-10-01 23:57:35 +0300

    the creation of buttons has been combined into one function, the header has been changed

--- qt_calculator/calculator.cpp
+++ qt_calculator/calculator.cpp
@@ -30,17 +30,13 @@ Calculator::Calculator(QWidget *parent)
     display->setAlignment(Qt::AlignRight);
     display->setFont(QFont("Calculator"));
 
-    QPushButton *btn7 = new QPushButton("7", this);
-    QPushButton *btn8 = new QPushButton("8", this);
-    QPushButton *btn9 = new QPushButton("9", this);
+    QPushButton *digit_buttons[10];
+    for (int i = 1; i < 10; i++) {
+        digit_buttons[i] = new QPushButton(QString::number(i), this);
+    }
+
     QPushButton *btn_divide = new QPushButton("/", this);
-    QPushButton *btn4 = new QPushButton("4", this);
-    QPushButton *btn5 = new QPushButton("5", this);
-    QPushButton *btn6 = new QPushButton("6", this);
     QPushButton *btn_multiply = new QPushButton("*", this);
-    QPushButton *btn1 = new QPushButton("1", this);
-    QPushButton *btn2 = new QPushButton("2", this);
-    QPushButton *btn3 = new QPushButton("3", this);
     QPushButton *btn_procent = new QPushButton("%", this);
     QPushButton *btn_minus = new QPushButton("-", this);
     QPushButton *btn0 = new QPushButton("0", this);
@@ -53,121 +49,38 @@ Calculator::Calculator(QWidget *parent)
     QGridLayout *gridLayout = new QGridLayout(this);
     gridLayout->addWidget(display, 0, 0, 1, 4);
 
+    for(int i = 1; i < 10; i++) {
+        int row = ((9 - i) / 3) + 2;
+        int column = ((i - 1) % 3);
+        gridLayout->addWidget(digit_buttons[i], row, column);
+    }
+
     gridLayout->addWidget(btn_clear, 1, 0);
     gridLayout->addWidget(btn_procent, 1, 2);
     gridLayout->addWidget(btn_delete, 1, 1);
     gridLayout->addWidget(btn_divide, 1, 3);
-
-    gridLayout->addWidget(btn7, 2, 0);
-    gridLayout->addWidget(btn8, 2, 1);
-    gridLayout->addWidget(btn9, 2, 2);
     gridLayout->addWidget(btn_multiply, 2, 3);
-
-    gridLayout->addWidget(btn4, 3, 0);
-    gridLayout->addWidget(btn5, 3, 1);
-    gridLayout->addWidget(btn6, 3, 2);
     gridLayout->addWidget(btn_minus, 3, 3);
-
-    gridLayout->addWidget(btn1, 4, 0);
-    gridLayout->addWidget(btn2, 4, 1);
-    gridLayout->addWidget(btn3, 4, 2);
     gridLayout->addWidget(btn_plus, 4, 3);
-
     gridLayout->addWidget(btn0, 5, 0, 1, 2);
     gridLayout->addWidget(btn_dot, 5, 2);
     gridLayout->addWidget(btn_equal, 5, 3);
 
     setLayout(gridLayout);
 
-    btn7->setStyleSheet(
-        "border-radius: 43%; "
-        "background: rgb(87, 92, 130); "
-        "color: #fff; "
-        "padding: 1px; "
-        "text-align: center; "
-        "width: 84px; "
-        "height: 84px; "
-        "font-size: 20px; "
-        "font-weight: 700; ");
-    btn8->setStyleSheet(
-        "border-radius: 43%; "
-        "background: rgb(87, 92, 130); "
-        "color: #fff; padding: 1px; "
-        "text-align: center; "
-        "width: 84px; "
-        "height: 84px; "
-        "font-size: 20px; "
-        "font-weight: 700; ");
-    btn9->setStyleSheet(
-        "border-radius: 43%; "
-        "background: rgb(87, 92, 130); "
-        "color: #fff; "
-        "padding: 1px; "
-        "text-align: center; "
-        "width: 84px; "
-        "height: 84px; "
-        "font-size: 20px; "
-        "font-weight: 700; ");
-    btn6->setStyleSheet(
-        "border-radius: 43%; "
-        "background: rgb(87, 92, 130); "
-        "color: #fff; "
-        "padding: 1px; "
-        "text-align: center; "
-        "width: 84px; "
-        "height: 84px; "
-        "font-size: 20px; "
-        "font-weight: 700; ");
-    btn5->setStyleSheet(
-        "border-radius: 43%; "
-        "background: rgb(87, 92, 130); "
-        "color: #fff; "
-        "padding: 1px; "
-        "text-align: center; "
-        "width: 84px; "
-        "height: 84px; "
-        "font-size: 20px; "
-        "font-weight: 700; ");
-    btn4->setStyleSheet(
-        "border-radius: 43%; "
-        "background: rgb(87, 92, 130); "
-        "color: #fff; "
-        "padding: 1px; "
-        "text-align: center; "
-        "width: 84px; "
-        "height: 84px; "
-        "font-size: 20px; "
-        "font-weight: 700; ");
-    btn3->setStyleSheet(
-        "border-radius: 43%; "
-        "background: rgb(87, 92, 130); "
-        "color: #fff; "
-        "padding: 1px; "
-        "text-align: center; "
-        "width: 84px; "
-        "height: 84px; "
-        "font-size: 20px; "
-        "font-weight: 700; ");
-    btn2->setStyleSheet(
-        "border-radius: 43%; "
-        "background: rgb(87, 92, 130); "
-        "color: #fff; "
-        "padding: 1px; "
-        "text-align: center; "
-        "width: 84px; "
-        "height: 84px; "
-        "font-size: 20px; "
-        "font-weight: 700; ");
-    btn1->setStyleSheet(
-        "border-radius: 43%; "
-        "background: rgb(87, 92, 130); "
-        "color: #fff; "
-        "padding: 1px; "
-        "text-align: center; "
-        "width: 84px; "
-        "height: 84px; "
-        "font-size: 20px; "
-        "font-weight: 700; ");
+    for (int i = 1; i < 10; i++) {
+        digit_buttons[i]->setStyleSheet(
+                                        "border-radius: 43%; "
+                                        "background: rgb(87, 92, 130); "
+                                        "color: #fff; "
+                                        "padding: 1px; "
+                                        "text-align: center; "
+                                        "width: 84px; "
+                                        "height: 84px; "
+                                        "font-size: 20px; "
+                                        "font-weight: 700; ");
+    }
+
     btn_minus->setStyleSheet(
         "border-radius: 43%; "
         "background: rgb(231,187,103); "
@@ -269,15 +182,9 @@ Calculator::Calculator(QWidget *parent)
         "font-size: 20px; "
         "font-weight: 700; ");
 
-    connect(btn9, &QPushButton::clicked, this, &Calculator::digit_clicked);
-    connect(btn8, &QPushButton::clicked, this, &Calculator::digit_clicked);
-    connect(btn7, &QPushButton::clicked, this, &Calculator::digit_clicked);
-    connect(btn6, &QPushButton::clicked, this, &Calculator::digit_clicked);
-    connect(btn5, &QPushButton::clicked, this, &Calculator::digit_clicked);
-    connect(btn4, &QPushButton::clicked, this, &Calculator::digit_clicked);
-    connect(btn3, &QPushButton::clicked, this, &Calculator::digit_clicked);
-    connect(btn2, &QPushButton::clicked, this, &Calculator::digit_clicked);
-    connect(btn1, &QPushButton::clicked, this, &Calculator::digit_clicked);
+    for (int i = 1; i < 10; ++i) {
+        connect(digit_buttons[i], &QPushButton::clicked, this, &Calculator::digit_clicked);
+    }
     connect(btn0, &QPushButton::clicked, this, &Calculator::digit_clicked);
     connect(btn_dot, &QPushButton::clicked, this, &Calculator::digit_clicked);
 }
--- qt_calculator/calculator.h
+++ qt_calculator/calculator.h
@@ -22,17 +22,8 @@ public:
 private:
     Ui::Calculator *ui;
     QLineEdit *display;
-    QPushButton *btn7;
-    QPushButton *btn8;
-    QPushButton *btn9;
     QPushButton *btn_divide;
-    QPushButton *btn4;
-    QPushButton *btn5;
-    QPushButton *btn6;
     QPushButton *btn_multiply;
-    QPushButton *btn1;
-    QPushButton *btn2;
-    QPushButton *btn3;
     QPushButton *btn_procent;
     QPushButton *btn_minus;
     QPushButton *btn0;
@@ -41,6 +32,7 @@ private:
     QPushButton *btn_plus;
     QPushButton *btn_clear;
     QPushButton *btn_delete;
+    QPushButton *digit_buttons[];
 
 private slots:
     void digit_clicked();
